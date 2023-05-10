9c2dcbba19cf12b678e68a0e47c5222f50315f64
Matt Cuadros <mcuadrose@gmail.com>
2023-05-10 00:12:29 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const { read, register } = require("./operations.js");
+
+/*  */
+const arguments = process.argv.slice(2);
+
+console.log("Los argumentos son: ", arguments);
+
+const action = arguments[0];
+
+const name = arguments[1];
+const age = arguments[2];
+const animal = arguments[3];
+const color = arguments[4];
+const diagnostic = arguments[5];
+
+if (action === "register") {
+  if (
+    name === undefined ||
+    age === undefined ||
+    animal === undefined ||
+    color === undefined ||
+    diagnostic === undefined
+  ) {
+    return console.log("⚠ Please fill in all the fields. ❗");
+  } else {
+    register(name, age, animal, color, diagnostic);
+  }
+} else if (action === "read") {
+  read();
+} else {
+  return console.log("Invalid Action");
+}
---
+++ operations.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+
+const register = (name, age, animal, color, diagnostic) => {
+  const filename = "meetings.json";
+  const database = JSON.parse(fs.readFileSync(filename, "utf8"));
+  console.log(database);
+  database.push({ id: Date.now(), name, age, animal, color, diagnostic });
+  console.log(`${name} - ${animal} Registered successfull! ✅`);
+  fs.writeFileSync(filename, JSON.stringify(database));
+};
+
+const read = () => {
+  const filename = "meetings.json";
+  console.log(JSON.parse(fs.readFileSync(filename, "utf8")));
+};
+
+module.exports = { register, read };
